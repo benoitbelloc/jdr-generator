17c81ab0ca41979b3ea72e30c63d58f3622f1e98
benoitbelloc <benoit.belloc@ynov.com>
2023-03-23 16:52:26 +0100

    css a modifier !

--- src/views/home/Home.tsx
+++ src/views/home/Home.tsx
@@ -2,7 +2,7 @@ import { Character as CharacterType } from '../main-content/MainContent';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
-import { CardActionArea } from '@mui/material';
+import { CardActionArea, Divider } from '@mui/material';
 import './Home.css';
 import { Link } from "react-router-dom";
 
@@ -10,24 +10,25 @@ function Home(props: {list: CharacterType[]}) {
 
   return (
     <div className="character" style={{width: '90%', height: '90%'}}>
-      <Card className="card">
-        <CardActionArea>
+      
         {props.list.map((item: CharacterType, index: number)=>{
           return (
             <div key={index}>
-              <CardMedia className="cardMedia" component={Link} to={"/character/" + item.id} sx={{ display: 'block', color: 'black' }}>
-                <img src={item.infos.avatar} />
-              </CardMedia>
-              <CardContent className="cardContent">
-                <h3>{item.infos.name}</h3>
-                <h5>{item.infos.class}</h5>
-                <p>{item.description}</p>
-              </CardContent>
+              <Card className="cards">
+                <CardActionArea>
+                      <CardMedia className="cardMedia" component={Link} to={"/character/" + item.id}>
+                        <img className="cardImage" src={item.infos.avatar} />
+                        <CardContent className="cardContent">
+                          <h3>{item.infos.name}</h3>
+                          <h5>{item.infos.class}</h5>
+                          <p>{item.description}</p>
+                        </CardContent>
+                      </CardMedia>
+                    </CardActionArea>
+              </Card>
             </div>
           )
         })}
-        </CardActionArea>
-      </Card>
     </div>
   )
 }
