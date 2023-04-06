795f6d92b5532946b18618427f5b524fcd0a40cc
Benoit Belloc <belloc.benoitjean@gmail.com>
2023-04-06 17:11:26 +0200

    then=>try

--- src/providers/characters-provider/CharactersProvider.tsx
+++ src/providers/characters-provider/CharactersProvider.tsx
@@ -41,17 +41,6 @@ function CharactersProvider ({children}: {children: React.ReactNode}) {
         }
     }
 
-    const getOneCharacter = async (id: string) => {
-        if (!id) return setCharacter(baseCharacter);
-        try {
-            const response = await fetch(`http://localhost:3000/characters/${id}`)
-            const data = await response.json()
-            setCharacter(data)
-        } catch (error) {
-            console.log(error);
-        }
-    }
-
     const updateOneCharacter = async (characterData: Character) => {
         console.log(characterData.id);
         try {
@@ -64,6 +53,30 @@ function CharactersProvider ({children}: {children: React.ReactNode}) {
                 body: JSON.stringify(characterData)
             })
             setCharacter(characterData)
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    const removeCharacter = async (id: string) => {
+        try {
+            await fetch(`http://localhost:3000/characters/${id}`, 
+            {
+                method: "DELETE",
+            })
+            setCharacter(baseCharacter)
+            getAllCharacters();
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    const getOneCharacter = async (id: string) => {
+        if (!id) return setCharacter(baseCharacter);
+        try {
+            const response = await fetch(`http://localhost:3000/characters/${id}`)
+            const data = await response.json()
+            setCharacter(data)
         } catch (error) {
             console.log(error);
         }
@@ -86,21 +99,18 @@ function CharactersProvider ({children}: {children: React.ReactNode}) {
         }
     }
 
-    const removeCharacter = async (id: string) => {
+    const getCharactersByUserId = async (userId: string) => {
         try {
-            await fetch(`http://localhost:3000/characters/${id}`, 
-            {
-                method: "DELETE",
-            })
-            setCharacter(baseCharacter)
-            getAllCharacters();
+            const response = await fetch(`http://localhost:3000/characters?userId=${userId}`)
+            const data = await response.json()
+            setCharacters(data)
         } catch (error) {
-            console.log(error);
+            console.log(error)
         }
     }
-    
+
     return (
-        <CharactersContext.Provider value={{ characters, character, getAllCharacters, getOneCharacter, updateOneCharacter, createOneCharacter, removeCharacter }}>
+        <CharactersContext.Provider value={{ characters, character, getAllCharacters, getOneCharacter, updateOneCharacter, createOneCharacter, removeCharacter, getCharactersByUserId }}>
             {children}
         </CharactersContext.Provider>
     )
